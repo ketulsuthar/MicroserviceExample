e18263d65137b1026eef26b5a0217b7e5e5b0572
ketulsuthar <k2lsuthar@gmail.com>
2021-10-30 14:50:20 -0400

    Updated retrive method for id there is not product

--- admin/products/views.py
+++ admin/products/views.py
@@ -17,10 +17,15 @@ class ProductViewSet(viewsets.ViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def retrieve(self, requrst, pk=None):
-        product = Product.objects.get(id=pk)
-        serializer = ProductSerializers(product)
-        return Response(serializer.data)
+    def retrieve(self, request, pk=None):
+        try:
+            product = Product.objects.get(id=pk)
+        except:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            print(product)
+            serializer = ProductSerializers(product)
+            return Response(serializer.data)
 
     def update(self, request, pk=None):
         product = Product.objects.get(id=pk)
@@ -30,4 +35,6 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
